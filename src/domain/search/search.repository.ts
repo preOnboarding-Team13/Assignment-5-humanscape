b74324cc7c37f7388e3f89d565563e4c271cc835
WonDongGyun <52685665+WonDongGyun@users.noreply.github.com>
2021-11-17 02:09:24 +0900

    Fix: 단순 임상 데이터 조회 에러 처리
    1. 단순 임상 데이터 조회 에러 처리
    에러 처리가 안돼있어서 처리하였습니다.

--- src/domain/search/search.repository.ts
+++ src/domain/search/search.repository.ts
@@ -1,13 +1,19 @@
 import { EntityRepository, Repository } from "typeorm";
 import { UpdatedTrials } from "../entities/updatedTrials.entity";
+import { NotFoundDataException } from "./exception/NotFoundDataException";
 
 @EntityRepository(UpdatedTrials)
 export class SearchRepository extends Repository<UpdatedTrials> {
 	async findTrials(trial_id: string) {
-		const result = await this.query(`select value
-from updateTrials, json_each(data)
-where json_extract(json_each.value, '$.trial_id') LIKE '%${trial_id}%'`);
+		const queryResult = await this.query(`
+			SELECT value
+			FROM updateTrials, json_each(data)
+			WHERE json_extract(json_each.value, '$.trial_id') LIKE '%${trial_id}%'`);
 
-		return JSON.parse(result[0].value);
+		if (queryResult.length === 0) {
+			throw new NotFoundDataException();
+		}
+
+		return JSON.parse(queryResult[0].value);
 	}
 }
